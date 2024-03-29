f129ecfe6917180e7a3ec43931e429a254439f9e
samosato <68155940+samuelgecik@users.noreply.github.com>
2024-03-29 17:37:23 +0100

    Update server.py layout and add country dropdown

--- server.py
+++ server.py
@@ -26,14 +26,13 @@ print(columns)
 df['Contact Address'] = df['Contact City'] + ', ' + df['Contact PCode'] + ', ' + df['Contact Address L1']
 df.drop(['Contact City', 'Contact PCode', 'Contact Address L1', 'Contact Address L2'], axis=1, inplace=True)
 
-# Define layout
 app.layout = dbc.Container(
     [
         dbc.Row(
             [
                 dbc.Col(
-                    html.H1("Data Filter", className="text-center text-primary, mb-4"),
-                    width=12,
+                    html.H1("EU ETS Registered Operators", className="text-center text-primary, mb-4"),
+                    width={"size": 12, "offset": 0},
                 )
             ]
         ),
@@ -47,6 +46,11 @@ app.layout = dbc.Container(
                             placeholder="Filter rows...",
                             className="mb-3",
                         ),
+                        dcc.Dropdown(
+                            id='country-dropdown',
+                            options=[{'label': i, 'value': i} for i in df['Contact Country'].unique()],
+                            placeholder="Select a country",
+                        ),
                         dbc.Button(
                             "Submit",
                             id="filter-button",
@@ -54,41 +58,47 @@ app.layout = dbc.Container(
                             className="mt-1",
                         ),
                     ],
-                    width=12,
+                    md={"size": 6, "offset": 3},
                 )
             ]
         ),
-        dbc.Row([dbc.Col(html.Div(id="table-container"), width=12)]),
-    ]
+        dbc.Row([dbc.Col(html.Div(id="table-container", style={'margin-top': '5px'}), md=12)]),
+    ],
+    fluid=True,
 )
 
 
 @app.callback(
     Output('table-container', 'children'),
     [Input('filter-button', 'n_clicks')],
-    [State('filter-input', 'value')]
+    [State('country-dropdown', 'value'), State('filter-input', 'value')]
 )
-def update_table(n_clicks, value):
-    if value is not None:
-        filtered_df = df[df.apply(lambda row: row.astype(str).str.contains(value).any(), axis=1)]
+def update_table(n_clicks, selected_country, input_value):
+    if selected_country is not None:
+        filtered_df = df[df['Contact Country'] == selected_country]
     else:
         filtered_df = df
+
+    if input_value is not None:
+        lowercased_value = input_value.lower()
+        filtered_df = filtered_df[filtered_df.apply(lambda row: row.astype(str).str.lower().str.contains(lowercased_value).any(), axis=1)]
 
     return [dash.dash_table.DataTable(
         data=filtered_df.to_dict('records'),
         columns=[{'name': i, 'id': i} for i in filtered_df.columns],
         page_action='native',
-        page_size=30,
+        page_size=50,
         virtualization=True,
         style_data_conditional=[
             {
                 'if': {'row_index': 'odd'},
                 'backgroundColor': 'lightgray'
             }
-        ]
+        ],
+        style_table={'minHeight': '800px', 'height': '800px', 'maxHeight': '800px'}
     )]
 
 
 # Run app
 if __name__ == "__main__":
-    app.run_server(port=3000)
+    app.run_server(host='0.0.0.0', port=3000)
